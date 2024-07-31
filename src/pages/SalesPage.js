2e39b6b011516b815692ffd03f8e325fd90ced60
Harshil Antala <117176501+harshilantala@users.noreply.github.com>
2024-07-31 21:49:59 +0530

    purchase and sales

---
+++ src/pages/SalesPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar2 from '../components/Navbar2';
+import Sales from '../components/Sales';
+
+const SalesPage = () => {
+  return (
+    <div>
+      <Navbar2 />
+      <br />
+      <Sales />
+      <br />
+    </div>
+  );
+};
+
+export default SalesPage;
