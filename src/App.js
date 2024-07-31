abc852b503b17fe2ffce919b0252a7ff97177f83
Harshil Antala <117176501+harshilantala@users.noreply.github.com>
2024-07-31 21:50:27 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import LoginForm from './components/Login';
 // import Purchase from './components/Purchase'
 import PurchasePage from './pages/PurchasePage';
+import SalesPage from './pages/SalesPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/purchase" element={<PurchasePage />} />
+        <Route path="/sale" element={<SalesPage />} />
       </Routes>
     </Router>
   );
