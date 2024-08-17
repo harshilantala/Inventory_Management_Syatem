d450ac2ec6b5aeb890b215c4806d88400e6fd327
Harshil Antala <117176501+harshilantala@users.noreply.github.com>
2024-08-17 22:49:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import LoginForm from './components/Login';
+// import LoginForm from './components/Login';
 // import Purchase from './components/Purchase'
 import PurchasePage from './pages/PurchasePage';
 import SalesPage from './pages/SalesPage';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import RegistrationForm from './components/RegistrationForm';
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginForm />} />
+        {/* <Route path="/login" element={<LoginForm />} /> */}
+        <Route path="/register" element={<RegistrationForm />} />
         <Route path="/purchase" element={<PurchasePage />} />
         <Route path="/sale" element={<SalesPage />} />
       </Routes>
