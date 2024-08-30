2e6053de901a6da5a31961b2a369fbf5c83cd9d7
Nand-Banugariya <152760341+Nand-Banugariya@users.noreply.github.com>
2024-08-30 11:27:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-// import LoginForm from './components/Login';
-// import Purchase from './components/Purchase'
+import LoginForm from './components/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import PurchasePage from './pages/PurchasePage';
 import SalesPage from './pages/SalesPage';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import RegistrationForm from './components/RegistrationForm';
-
+import OrderHistory from './components/OrderHistory';
+// Import the BillPage component
+import BillPage from './components/Billgenerate'; // Ensure the path is correct
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Define the routes */}
         <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<LoginForm />} /> */}
-        <Route path="/register" element={<RegistrationForm />} />
+        <Route path="/login" element={<LoginForm />} />
         <Route path="/purchase" element={<PurchasePage />} />
         <Route path="/sale" element={<SalesPage />} />
+        <Route path="/history" element={<OrderHistory />} />
+        {/* Add BillPage Route for generating bills */}
+        <Route path="/bill/:id" element={<BillPage />} />
       </Routes>
     </Router>
   );
